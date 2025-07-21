0b35b8570198fcae5c2eaadf9168c5658c4534f5
pallav kafle <157238903+pallavkafle@users.noreply.github.com>
2025-07-21 23:46:32 +0545

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -32,7 +32,7 @@ const navigate= useNavigate();
         }
         
         try{
-            const url = "http://localhost:8080/auth/login";
+            const url = "https://deploy-authapp.onrender.com/auth/login";
 
             const response = await fetch(url, {
                 method: "POST",
